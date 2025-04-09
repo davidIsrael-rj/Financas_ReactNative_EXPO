c4526f2a7083f9776c2ceba38c0ca138ef74bdc3
DavidIsrael <davidisrael.rj@gmail.com>
2025-04-09 10:21:57 -0300

    Iniciando projeto de Finanças

---
+++ app/+not-found.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+    <View>
+      <Text>Página não encontrada</Text>
+    </View>
+    </>
+  );
+}
---
+++ app/_layout.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+export default function RootLayout() {
+
+
+  return (
+    <View>
+      <Text>Bom dia</Text>
+      <Text>Iniciando projeto de Finanças</Text>
+    </View>
+  );
+}
