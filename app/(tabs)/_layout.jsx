9956ff2c8510646478f93799e2ae18670c58d1ea
DavidIsrael <davidisrael.rj@gmail.com>
2025-04-09 10:46:06 -0300

    Trocando o background da StatusBar

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,9 +1,12 @@
 import { Tabs } from "expo-router";
 import { Text, View } from "react-native";
+import { colors } from "../../constants/colors";
 
 export default function TabsLayout() {
   return (
-   <Tabs>
+   <Tabs screenOptions={{
+    headerStyle: {backgroundColor: colors.primary}
+   }}>
     <Tabs.Screen name="index"/>
     <Tabs.Screen name="add-transactions"/>
     <Tabs.Screen name="summary"/>
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,17 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { colors } from '../constants/colors';
 
 export default function RootLayout() {
 
 
   return (
+    <>
+    <StatusBar backgroundColor={colors.primary} style='light'/>
     <Stack>
       <Stack.Screen name='(tabs)' options={{headerShown: false}}/>
       <Stack.Screen name='+not-found'/>
     </Stack>
+    </>
   );
 }
