61f5e6885d6fd8ecc04ff6d71c71cd8a21026c18
DavidIsrael <davidisrael.rj@gmail.com>
2025-04-10 16:59:30 -0300

    GlobalState

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,17 +1,18 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { colors } from '../constants/colors';
+import GlobalState from '../contexts/GlobalState';
 
 export default function RootLayout() {
 
 
   return (
-    <>
-    <StatusBar backgroundColor={colors.primary} style='light'/>
-    <Stack>
-      <Stack.Screen name='(tabs)' options={{headerShown: false}}/>
-      <Stack.Screen name='+not-found'/>
-    </Stack>
-    </>
+    <GlobalState>
+      <StatusBar backgroundColor={colors.primary} style='light' />
+      <Stack>
+        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+        <Stack.Screen name='+not-found' />
+      </Stack>
+    </GlobalState>
   );
 }
---
+++ contexts/GlobalState.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react"
+
+export const MoneyContext = createContext()
+
+export default function GlobalState({children}) {
+    const [transactions, setTransactions] = useState(["ola", "mundo"])
+
+    return (
+        <MoneyContext.Provider
+            value={[transactions, setTransactions]}>
+            {children}
+        </MoneyContext.Provider>
+    )
+}
