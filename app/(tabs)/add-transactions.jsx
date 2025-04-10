be86ca563781faddac0344301279e0e2eda571a7
DavidIsrael <davidisrael.rj@gmail.com>
2025-04-10 11:35:57 -0300

    Adicionando o valores no estado e exibindo no alert

--- app/(tabs)/add-transactions.jsx
+++ app/(tabs)/add-transactions.jsx
@@ -1,11 +1,21 @@
 import { Alert, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import { globalStyles } from "../../styles/globalStyles";
 import Button from "../../components/Button";
+import { useState } from "react";
 
 export default function AddTransactions() {
 
+    const initialForm = {
+        description: "",
+        value: 0,
+        date: "",
+        category: "Renda"
+    }
+
+    const [form, setForm] = useState(initialForm)
+
     const addTransaction = () => {
-        Alert.alert("Bom dia!!")
+        Alert.alert(`${form.description} , ${form.value} , ${form.date} , ${form.category} `)
     }
     return (
         <SafeAreaView style={globalStyles.screenContainer}>
@@ -13,19 +23,33 @@ export default function AddTransactions() {
                 <View style={styles.form}>
                     <View>
                         <Text style={globalStyles.inputLabel}>Descrição</Text>
-                        <TextInput style={globalStyles.input} />
+                        <TextInput
+                            value={form.description}
+                            onChangeText={(text) => setForm({ ...form, description: text })}
+                            style={globalStyles.input}
+                        />
                     </View>
                     <View>
                         <Text style={globalStyles.inputLabel}>Valor</Text>
-                        <TextInput style={globalStyles.input} />
+                        <TextInput
+                            value={form.value}
+                            keyboardType="numeric"
+                            onChangeText={(text) => setForm({ ...form, value: text })}
+                            style={globalStyles.input} />
                     </View>
                     <View>
                         <Text style={globalStyles.inputLabel}>Data</Text>
-                        <TextInput style={globalStyles.input} />
+                        <TextInput
+                            value={form.date}
+                            onChangeText={(text) => setForm({ ...form, date: text })}
+                            style={globalStyles.input} />
                     </View>
                     <View>
                         <Text style={globalStyles.inputLabel}>Categoria</Text>
-                        <TextInput style={globalStyles.input} />
+                        <TextInput
+                            value={form.category}
+                            onChangeText={(text) => setForm({ ...form, category: text })}
+                            style={globalStyles.input} />
                     </View>
                 </View>
                 <Button onPress={addTransaction}>Adicionar</Button>
