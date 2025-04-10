fec9399e30b4c02b539946171cf2d811371d9d68
DavidIsrael <davidisrael.rj@gmail.com>
2025-04-10 11:15:10 -0300

    Criando o Botão de Adicionar, estilo global, formulário da tela de adicionar transação

--- app/(tabs)/add-transactions.jsx
+++ app/(tabs)/add-transactions.jsx
@@ -1,7 +1,43 @@
-import { Text } from "react-native";
+import { Alert, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { globalStyles } from "../../styles/globalStyles";
+import Button from "../../components/Button";
 
-export default function AddTransactions(){
+export default function AddTransactions() {
+
+    const addTransaction = () => {
+        Alert.alert("Bom dia!!")
+    }
     return (
-        <Text>Adicionar Transações</Text>
+        <SafeAreaView style={globalStyles.screenContainer}>
+            <ScrollView style={globalStyles.content}>
+                <View style={styles.form}>
+                    <View>
+                        <Text style={globalStyles.inputLabel}>Descrição</Text>
+                        <TextInput style={globalStyles.input} />
+                    </View>
+                    <View>
+                        <Text style={globalStyles.inputLabel}>Valor</Text>
+                        <TextInput style={globalStyles.input} />
+                    </View>
+                    <View>
+                        <Text style={globalStyles.inputLabel}>Data</Text>
+                        <TextInput style={globalStyles.input} />
+                    </View>
+                    <View>
+                        <Text style={globalStyles.inputLabel}>Categoria</Text>
+                        <TextInput style={globalStyles.input} />
+                    </View>
+                </View>
+                <Button onPress={addTransaction}>Adicionar</Button>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    form: {
+        gap: 12,
+        marginBottom: 40,
+        marginTop: 10,
+    }
+})
